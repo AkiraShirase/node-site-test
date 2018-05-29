5957f574b7256ecda3a5da070a007f3ba077b75d
grey <greytombs@yahoo.com>
2018-05-29 22:03:13 +0300

    create: nuxt.js project

---
+++ components/MainHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="main-header fixed-top">
+      <div class="row">
+        <div class="col-4"><div class="border border-bottom border-dark"><a class="text-center" href="#">home</a></div></div>
+        <div class="col-4"><div class="border border-bottom border-dark"><a class="text-center" href="#">contacts</a></div></div>
+        <div class="col-4"><div class="border border-bottom border-dark"><a class="text-center" href="#">gallery</a></div></div>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MainHeader"
+    }
+</script>
+
+<style scoped>
+
+</style>
